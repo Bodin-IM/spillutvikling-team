431b93dc5a3588f730de6c8c3f7debc4de53c5fa
Andreas Skarmark-Jakobsen <82838374+Andreas-SJ@users.noreply.github.com>
2024-12-17 10:51:20 +0100

    made a player class and added a placeholder image

---
+++ sprites.py
@@ -0,0 +1,25 @@
+import pygame as pg
+import main
+
+placeholder = True
+placeholderImg = pg.image.load("assets\sprites\placeholder.png")
+
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = placeholderImg
+        self.rect = self.image.get_rect()
+
+        self.game = placeholder
+
+        self.speed = 3
+
+    def update(self):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K] or keys[pg.K_RIGHT]:
+            self.rect.x += 2 * self.speed
+        
+        if keys[pg.K_a] or keys[pg.K_LEFT]:
+            self.rect.x -= 2 * self.speed
+
