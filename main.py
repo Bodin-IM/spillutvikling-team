0ab9a165ca405c640574576edefb12d27a6fa295
somoneisme <natanielellefsen@gmail.com>
2024-12-19 09:05:01 +0100

    terrain added

--- main.py
+++ main.py
@@ -5,31 +5,34 @@ from sprites import *
 class game():
     def __init__(self):
         pg.init()
-        screenWidth = 1080
-        screenHeight = 1080
+        self.screenWidth = 1080
+        self.screenHeight = 1080
         self.clock = pg.time.Clock()
-        self.screen = pg.display.set_mode((screenWidth,screenHeight))
+        self.screen = pg.display.set_mode((self.screenWidth,self.screenHeight))
         self.new()
 
 
     def new(self):
-        self.allsprites = pg.sprite.Group
-        self.enemyGroup = pg.sprite.Group
-        self.playerGroup = pg.sprite.Group
+        self.allsprites = pg.sprite.Group()
+        self.enemyGroup = pg.sprite.Group()
+        self.playerGroup = pg.sprite.Group()
         self.player = Player()
         self.allsprites.add(self.player)
-
+        self.terrain = terrain(self)
+        self.allsprites.add(self.terrain)
         self.run()
+
     def run(self):
         playing = True
         while playing:
             self.clock.tick(120)
             for event in pg.event.get():
-                if event == pg.QUIT:
+                if event.type == pg.QUIT:
                     playing = False
-            self.allsprites.update
-            self.allsprites.draw
+            self.allsprites.update()
+            self.allsprites.draw(self.screen)
 
+            pg.display.update()
 
 
 
--- sprites.py
+++ sprites.py
@@ -1,7 +1,9 @@
 import pygame as pg
+from random import randint
 
 placeholder = True
 placeholderImg = pg.image.load("assets\sprites\placeholder.png")
+terrainPlaceHolderImg = pg.image.load("assets\sprites/terrainPlaceholderImg.png")
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
@@ -22,3 +24,15 @@ class Player(pg.sprite.Sprite):
         if keys[pg.K_a] or keys[pg.K_LEFT]:
             self.rect.x -= 2 * self.speed
 
+class terrain(pg.sprite.Sprite):
+    def __init__(self,game):        
+        pg.sprite.Sprite.__init__(self)        
+        terrainSize = randint(20,100)
+        self.image = pg.transform.scale(terrainPlaceHolderImg,(terrainSize,terrainSize))
+
+        self.rect = self.image.get_rect()
+        self.rect.y = randint(0,game.screenHeight)
+        self.rect.x = randint(0,game.screenWidth)
+
+    def update(self):
+        pass
